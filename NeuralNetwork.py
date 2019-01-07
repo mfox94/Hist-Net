657c083817f3b9fcfdaf0407489ea79a8441db3d
mfox94 <45456634+mfox94@users.noreply.github.com>
2019-01-07 16:53:55 +0100

    Inizio Progetto sistemato

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -10,6 +10,7 @@ import seaborn as sns
 import cv2
 from scipy.misc import imresize, imread
 import sklearn
+from skimage import color
 from sklearn import model_selection
 from sklearn.model_selection import train_test_split, KFold, cross_val_score, StratifiedKFold, learning_curve, \
     GridSearchCV
@@ -22,138 +23,161 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC, LinearSVC
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 import keras
+from sklearn import datasets, svm, metrics
+from skimage import filters
 from keras import backend as K
 from keras.callbacks import Callback, EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils.np_utils import to_categorical
 from keras.models import Sequential, model_from_json
 from keras.optimizers import SGD, RMSprop, Adam, Adagrad, Adadelta
-from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization, Conv2D, MaxPool2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization, Conv2D, MaxPool2D, MaxPooling2D, \
+    AveragePooling2D
 from os import listdir
 import gc
-import skimage
 import time as time
 from keras.utils.np_utils import to_categorical
-
 import time as time
 
+# Colab paths...
+#  PATH_INPUT = "../Data/"
 
-class MetricsCheckpoint(Callback):
-    """Callback that saves metrics after each epoch"""
-
-    def __init__(self, savepath):
-        super(MetricsCheckpoint, self).__init__()
-        self.savepath = savepath
-        self.history = {}
-
-    def on_epoch_end(self, epoch, logs=None):
-        for k, v in logs.items():
-            self.history.setdefault(k, []).append(v)
-        np.save(self.savepath, self.history)
+# Local paths..
+PATH_INPUT = "../Preprocessed_Data/"
 
 
-def plotKerasLearningCurve():
-    plt.figure(figsize=(10, 5))
-    metrics = np.load('logs.npy')[()]
-    filt = ['acc']  # try to add 'loss' to see the loss learning curve
-    for k in filter(lambda x: np.any([kk in x for kk in filt]), metrics.keys()):
-        l = np.array(metrics[k])
-        plt.plot(l, c='r' if 'val' not in k else 'b', label='val' if 'val' in k else 'train')
-        x = np.argmin(l) if 'loss' in k else np.argmax(l)
-        y = l[x]
-        plt.scatter(x, y, lw=0, alpha=0.25, s=100, c='r' if 'val' not in k else 'b')
-        plt.text(x, y, '{} = {:.4f}'.format(x, y), size='15', color='r' if 'val' not in k else 'b')
-    plt.legend(loc=4)
-    plt.axis([0, None, None, None]);
-    plt.grid()
-    plt.xlabel('Number of epochs')
-
-
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    plt.figure(figsize=(5, 5))
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=90)
-    plt.yticks(tick_marks, classes)
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-
-
-def plot_learning_curve(history):
-    plt.figure(figsize=(8, 8))
-    plt.subplot(1, 2, 1)
+def printAcc(history):
     plt.plot(history.history['acc'])
     plt.plot(history.history['val_acc'])
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig('./accuracy_curve.png')
-    # plt.clf()
-    # summarize history for loss
-    plt.subplot(1, 2, 2)
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig('./loss_curve.png')
+    plt.show()
 
 
-start = time.time()
-print("starting..")
-print("loading images...")
-images = np.load('images.npy', mmap_mode=None, allow_pickle=True, fix_imports=True, encoding='ASCII')
-print("loading labels...")
-labels = np.load('labels.npy', mmap_mode=None, allow_pickle=True, fix_imports=True, encoding='ASCII')
-print("splitting..")
+def ApplyFilter(images, filter):
+    if filter == 'GREY':
+        Applygrey(images)
+    if filter == 'SOBEL':
+        Applysobel(images)
+    if filter == 'ROBERTS':
+        Applysobel(images)
+    if filter != 'GREY' | filter != 'GREY' | filter != 'GREY':
+        print("ERROR: unrecognized filter")
 
-images_bw = np.ndarray(shape=(90000, 50, 50))
-for i in range(images.shape[0]):
-     curr_im = curr_im.astype(np.uint8)
-     curr_im = skimage.color.rgb2gray(images[i])
-     images_bw[i] = curr_im
 
-# Images_Train, Images_test, Labels_Train, Labels_Test = train_test_split(images, labels, test_size=0.2)
-# print("to categorical Train")
-# y_trainHot = to_categorical(Labels_Train, num_classes=2)
-# print("to categorical Test")
-# y_testHot = to_categorical(Labels_Test, num_classes=2)
-#
-# batch_size = 128
+def Applysobel(images):
+    images_temp = np.ndarray(shape=(90000, 50, 50))
+    for i in range(images.shape[0]):
+        curr_im = images[i].astype('uint8')
+        curr_im = filters.sobel(color.rgb2gray(images[i]))
+        images_temp[i] = curr_im
+    return images_temp
+
+
+def Applygrey(images):
+    images_temp = np.ndarray(shape=(90000, 50, 50))
+    for i in range(images.shape[0]):
+        curr_im = images[i].astype('uint8')
+        curr_im = color.rgb2gray(images[i])
+        images_temp[i] = curr_im
+    return images_temp
+
+
+def Applyroberts(images):
+    images_temp = np.ndarray(shape=(90000, 50, 50))
+    for i in range(images.shape[0]):
+        curr_im = images[i].astype('uint8')
+
+        curr_im = filters.roberts(color.rgb2gray(images[i]))
+        images_temp[i] = curr_im
+    return images_temp
+
+
+def initialize():
+    print("loading images...")
+    images = np.load(PATH_INPUT + 'images.npy', mmap_mode=None, allow_pickle=True, fix_imports=True,
+                     encoding='ASCII')
+    print("loading labels...")
+    labels = np.load(PATH_INPUT + 'labels.npy', mmap_mode=None, allow_pickle=True, fix_imports=True,
+                     encoding='ASCII')
+    print("splitting..")
+    Images_Train, Images_test, Labels_Train, Labels_Test = train_test_split(images, labels, test_size=0.2)
+    print("to categorical Train")
+    y_trainHot = to_categorical(Labels_Train, num_classes=2)
+    print("to categorical Test")
+    y_testHot = to_categorical(Labels_Test, num_classes=2)
+    print("splitting completed!")
+    return Images_Train, Images_test, y_trainHot, y_testHot
+    del images
+    del labels
+
+
+def Hist_Net(num_classes, input_shape, strides):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape, strides=strides))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(128, (5, 5), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(AveragePooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+    datagen = ImageDataGenerator(
+        rotation_range=50,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.3,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.3,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=True)  # randomly flip images
+    return model, datagen
+
+
+def Hist_Net_Initialize(Images_Train, Images_test, y_trainHot, y_testHot, n_channel):
+    num_classes = 2
+    img_rows, img_cols = 50, 50
+    input_shape = (img_rows, img_cols, n_channel)
+    strides = 2
+    model, datagen = Hist_Net(num_classes, input_shape, strides)
+
+
+def main():
+    print("Initialization")
+    Images_Train, Images_test, y_trainHot, y_testHot = initialize()
+    num_channels = Images_Train.shape[3]  # check the channels of each image
+    Hist_Net_Initialize(Images_Train, Images_test, y_trainHot, y_testHot, num_channels)
+
+    print("COMPLETE!_______________")
+
+
+# print("MODEL HISTNET:")
+# batch_size = 1024
 # num_classes = 2
 # epochs = 8
-# img_rows, img_cols = 50, 50
+# img_rows,img_cols=50,50
 # input_shape = (img_rows, img_cols, 3)
+# #input_shape = (img_rows, img_cols, 1)
 # e = 2
 #
-# print("creating CNN")
+#
 # model = Sequential()
 # model.add(Conv2D(32, kernel_size=(3, 3),
 #                  activation='relu',
-#                  input_shape=input_shape, strides=e))
+#                  input_shape=input_shape,strides=e))
 # model.add(Conv2D(64, (3, 3), activation='relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.25))
+# model.add(Conv2D(128, (5, 5), activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Conv2D(128, (3, 3), activation='relu'))
+# model.add(AveragePooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.5))
 # model.add(Flatten())
 # model.add(Dense(128, activation='relu'))
 # model.add(Dropout(0.5))
@@ -162,28 +186,32 @@ for i in range(images.shape[0]):
 #               optimizer=keras.optimizers.Adadelta(),
 #               metrics=['accuracy'])
 #
-# print("creating DataGen")
-# datagen = ImageDataGenerator(
-#     featurewise_center=False,  # set input mean to 0 over the dataset
-#     samplewise_center=False,  # set each sample mean to 0
-#     featurewise_std_normalization=False,  # divide inputs by std of the dataset
-#     samplewise_std_normalization=False,  # divide each input by its std
-#     zca_whitening=False,  # apply ZCA whitening
-#     rotation_range=20,  # randomly rotate images in the range (degrees, 0 to 180)
-#     width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
-#     height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
-#     horizontal_flip=True,  # randomly flip images
-#     vertical_flip=True)  # randomly flip images
 # a = Images_Train
+# #a = Images_Train.reshape(Images_Train.shape[0], 50, 50, 1)
 # b = y_trainHot
 # c = Images_test
+# #c = Images_test.reshape(Images_test.shape[0], 50, 50, 1)
 # d = y_testHot
 # epochs = 10
 #
-# print("FITTING....")
-# history = model.fit_generator(datagen.flow(a,b, batch_size=32),
-#                         steps_per_epoch=len(a) / 32,
-#                               epochs=epochs,validation_data = [c, d],
-#                               callbacks = [MetricsCheckpoint('logs')])
+# print("Model 1 compiled!")
+# time1= time.time()
+#
+# datagen = ImageDataGenerator(
+#         rotation_range=50,  # randomly rotate images in the range (degrees, 0 to 180)
+#         width_shift_range=0.3,  # randomly shift images horizontally (fraction of total width)
+#         height_shift_range=0.3,  # randomly shift images vertically (fraction of total height)
+#         horizontal_flip=True,  # randomly flip images
+#         vertical_flip=True)  # randomly flip images
 #
 #
+# exec_model = model.fit_generator(datagen.flow(a,b, batch_size=16),
+#                         steps_per_epoch=len(a) / 16,
+#                               epochs=epochs,validation_data = [c, d])
+#
+# print("execution succeded in ")
+# print(time.time()-time1)
+# printAcc(exec_model)
+
+if __name__ == '__main__':
+    main()
